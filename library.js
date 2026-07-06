f7b1c44393c197724b4a2a0dd7a637c8a034c641
Fernanda Ervan Rustyawan <fernandaervan@gmail.com>
2026-07-06 16:56:46 +0700

    add sayGuest() function

--- library.js
+++ library.js
@@ -5,11 +5,3 @@ function sayHello(name) {
 function sayGuest() {
   return "Hello";
 }
-
-function sayHi(name) {
-  return "Hai" + name;
-}
-
-function sayErvan(name) {
-  return "Hello Ervan" + name;
-}
